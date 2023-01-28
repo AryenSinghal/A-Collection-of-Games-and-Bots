2218b8edbfe5aad10a8cc53860127d989f8f7199
Aryen Singhal <aryensinghal@gmail.com>
2023-01-28 12:26:38 +0530

    update menu ui

--- main.py
+++ main.py
@@ -6,10 +6,14 @@ from twentyfortyeight.AI import runAI
 from threading import Thread
 
 pygame.init()
+pygame.display.set_caption('A Collection of Games and Bots')
 
 brinjal = pygame.font.SysFont('Comic Sans',25)
-screen = pygame.display.set_mode((int(13.5*35), 9*35))
+screen = pygame.display.set_mode((int(13.5*35), int(16*35)))
 clock = pygame.time.Clock()
+
+menu_text = brinjal.render('MENU',True,'#FFFFFF')
+menu_text_rect = menu_text.get_rect(center = (int(13.5*35/2), 40))
 
 class Button:
 	def __init__(self,text,width,height,pos,elevation):
@@ -62,6 +66,28 @@ class Button:
 	def press(self):
 		print('click')
 
+class ImageButton(Button):
+	def __init__(self, image, text, width, height, pos, elevation):
+		super().__init__(text, width, height, pos, elevation)
+		self.image = pygame.image.load(image).convert_alpha()
+		self.image = pygame.transform.scale(self.image, (width, height))
+		self.image_rect = self.image.get_rect(center = self.top_rect.center)
+
+	def draw(self):
+		# elevation logic 
+		self.top_rect.y = self.original_y_pos - self.dynamic_elecation
+		self.image_rect.center = self.top_rect.center 
+		self.text_rect.center = self.top_rect.center
+
+		self.bottom_rect.midtop = self.top_rect.midtop
+		self.bottom_rect.height = self.top_rect.height + self.dynamic_elecation
+
+		pygame.draw.rect(screen,self.bottom_color, self.bottom_rect,border_radius = 12)
+		pygame.draw.rect(screen,self.top_color, self.top_rect,border_radius = 12)
+		screen.blit(self.image, self.image_rect)
+		screen.blit(self.text_surf, self.text_rect)
+		self.check_click()
+
 def on2048PressGame():
 	runGame(True)
 
@@ -87,15 +113,15 @@ def onClickAIPress():
 		i += 1
 
 run = True
-_2048Game = Button('2048 Game',200,40,(20, 20),5)
-_2048AI = Button('2048 AI',200,40,(250, 20),5)
-TicTacToe = Button('TicTacToe',200,40,(20, 100),5)
-MazeAI = Button('Maze AI',200,40,(250, 100),5)
-Snek = Button('Snek',200,40,(20, 180),5)
-SnekAI = Button('SnekAI',200,40,(250, 180),5)
-ClickGame = Button('Cookie clicker',200,40,(20, 260),5)
-ClickAI = Button('Cookie clicker AI',200,40,(250, 260),5)
-bootons = [_2048Game, _2048AI, TicTacToe, MazeAI, Snek, SnekAI, ClickGame, ClickAI]
+_2048Game = Button('2048 Game',200,40,(20, 100),5)
+_2048AI = Button('2048 AI',200,40,(250, 100),5)
+TicTacToe = Button('TicTacToe',200,40,(20, 180),5)
+MazeAI = Button('Maze AI',200,40,(250, 180),5)
+Snek = Button('Snek',200,40,(20, 260),5)
+SnekAI = Button('SnekAI',200,40,(250, 260),5)
+ClickGame = ImageButton('assets/cookie.png','Cookie clicker',200,200,(135, 340),5)
+ClickAI = Button('Cookie clicker AI',200,40,(250, 340),5)
+bootons = [_2048Game, _2048AI, TicTacToe, MazeAI, Snek, SnekAI, ClickGame]
 
 _2048Game.press = on2048PressGame
 _2048AI.press = on2048PressAI
@@ -111,6 +137,7 @@ while run:
             run = False
     
     screen.fill((30, 30, 30))
+    screen.blit(menu_text, menu_text_rect)
     for b in bootons: b.draw()
     pygame.display.update()
 
