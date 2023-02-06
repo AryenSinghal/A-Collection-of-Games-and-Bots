3c4fc64fa1b32984a67d8949d4ae718e2d3eef8b
zengarv <garv.jain835@gmail.com>
2023-02-06 23:30:43 +0530

    I can beat a cobra in a fistfight with a loaf of bread

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ class Button:
 		#Core attributes 
 		self.pressed = False
 		self.elevation = elevation
-		self.dynamic_elecation = elevation
+		self.dynamic_elevation = elevation
 		self.original_y_pos = pos[1]
 
 		# top rectangle 
@@ -36,11 +36,11 @@ class Button:
 
 	def draw(self):
 		# elevation logic 
-		self.top_rect.y = self.original_y_pos - self.dynamic_elecation
+		self.top_rect.y = self.original_y_pos - self.dynamic_elevation
 		self.text_rect.center = self.top_rect.center 
 
 		self.bottom_rect.midtop = self.top_rect.midtop
-		self.bottom_rect.height = self.top_rect.height + self.dynamic_elecation
+		self.bottom_rect.height = self.top_rect.height + self.dynamic_elevation
 
 		pygame.draw.rect(screen,self.bottom_color, self.bottom_rect,border_radius = 12)
 		pygame.draw.rect(screen,self.top_color, self.top_rect,border_radius = 12)
@@ -52,15 +52,15 @@ class Button:
 		if self.top_rect.collidepoint(mouse_pos):
 			self.top_color = '#D74B4B'
 			if pygame.mouse.get_pressed()[0]:
-				self.dynamic_elecation = 0
+				self.dynamic_elevation = 0
 				self.pressed = True
 			else:
-				self.dynamic_elecation = self.elevation
+				self.dynamic_elevation = self.elevation
 				if self.pressed == True:
 					self.press()
 					self.pressed = False
 		else:
-			self.dynamic_elecation = self.elevation
+			self.dynamic_elevation = self.elevation
 			self.top_color = '#475F77'
     
 	def press(self):
@@ -75,12 +75,12 @@ class ImageButton(Button):
 
 	def draw(self):
 		# elevation logic 
-		self.top_rect.y = self.original_y_pos - self.dynamic_elecation
+		self.top_rect.y = self.original_y_pos - self.dynamic_elevation
 		self.image_rect.center = self.top_rect.center 
 		self.text_rect.center = self.top_rect.center
 
 		self.bottom_rect.midtop = self.top_rect.midtop
-		self.bottom_rect.height = self.top_rect.height + self.dynamic_elecation
+		self.bottom_rect.height = self.top_rect.height + self.dynamic_elevation
 
 		pygame.draw.rect(screen,self.bottom_color, self.bottom_rect,border_radius = 36)
 		pygame.draw.rect(screen,self.top_color, self.top_rect,border_radius = 36)
@@ -117,8 +117,8 @@ _2048Game = Button('2048 Game',200,40,(20, 100),5)
 _2048AI = Button('2048 AI',200,40,(250, 100),5)
 TicTacToe = Button('TicTacToe',200,40,(20, 180),5)
 MazeAI = Button('Maze AI',200,40,(250, 180),5)
-Snek = Button('Snek',200,40,(20, 260),5)
-SnekAI = Button('SnekAI',200,40,(250, 260),5)
+Snek = Button('Snake',200,40,(20, 260),5)
+SnekAI = Button('SnakeAI',200,40,(250, 260),5)
 ClickGame = ImageButton('assets/cookie.png','Cookie clicker',200,200,(135, 340),5)
 ClickAI = Button('Cookie clicker AI',200,40,(250, 340),5)
 bootons = [_2048Game, _2048AI, TicTacToe, MazeAI, Snek, SnekAI, ClickGame]
